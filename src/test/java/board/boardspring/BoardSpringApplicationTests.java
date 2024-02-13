b122e3a31cc0680b5306a813c05c653833a5e5e7
ChoSooBeen <cba0613060@gmail.com>
2024-02-13 17:33:46 +0900

    JDBC를 이용한 데이터베이스 연결

--- src/test/java/board/boardspring/BoardSpringApplicationTests.java
+++ src/test/java/board/boardspring/BoardSpringApplicationTests.java
@@ -1,13 +1,30 @@
 package board.boardspring;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class BoardSpringApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
+	@Test
+	public void testJdbcConnection() {
+		jdbcTemplate.execute("CREATE TABLE IF NOT EXISTS test_table (id INT, name VARCHAR(255))");
+		jdbcTemplate.update("INSERT INTO test_table VALUES (1, 'Kim')");
+
+		String result = jdbcTemplate.queryForObject("SELECT name FROM test_table WHERE id = 1", String.class);
+
+		assertTrue("Kim".equals(result));
 	}
 
 }
